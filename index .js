4ef8302e1ff386a1ba5e03bab57342b45dc9303a
hrgohel <hrgohel2004@gmail.com>
2024-10-27 17:33:13 +0530

    added index.js

---
+++ index .js
@@ -0,0 +1,84 @@
+<html>
+	<head>
+	<header>
+	<style>
+	#mySearch {
+  width: 100%;
+  font-size: 18px;
+  padding: 9px;
+  border: 1px solid #ddd;
+}
+#myMenu {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+#myMenu li a {
+  padding: 22px;
+  text-decoration: none;
+  color: black;
+  display: block;
+  float:left;
+}
+#myMenu li a:hover {
+  background-color: #eee;
+}
+</style>
+</header>
+<body style="background: rgb(238,174,202);
+background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,173,233,0.7987570028011204) 100%);">
+<div style="width:100%;position:absolute;">
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zero Trust Architecture in cloud networks: application, challenges and future opportunities</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Zero Trust Architecture in cloud networks: application, challenges and future opportunities</h1>
+        <nav>
+            <ul>
+				<center>
+				<img src="D:\Harsh\OIP.jpg"/ style="width:250px; border:3px solid">
+				</center>
+				<li><a href="*c:\Users\Rajendra\Desktop\bca program\Bca  2 year\bca sem 3\Web technology\index.html"style="width:20%;color:white;float:left;text-align:center;font-family:bold;height:40;font-size:40">Index</a></li>
+				<li><a href="*c:\Users\Rajendra\Desktop\bca program\Bca  2 year\bca sem 3\Web technology\about.html"style="width:20%;color:white;float:left;text-align:center;font-family:bold;height:40;font-size:40">About</a></li>
+                <li><a href="*c:\Users\Rajendra\Desktop\bca program\Bca  2 year\bca sem 3\Web technology\Security.html"style="width:20%;color:white;float:left;text-align:center;font-family:bold;height:40;font-size:40">Security</a></li>
+                <li><a href="*c:\Users\Rajendra\Desktop\bca program\Bca  2 year\bca sem 3\Web technology\Meera.html"style="width:20%;color:white;float:left;text-align:center;font-family:bold;height:40;font-size:40">Contact</a></li>
+            </ul>
+             <div class="rightNav">
+	        <input type="text" name"search" id="seacrh">
+	          <button class ="btn btn-sm">search </button>
+
+        </nav>
+    </header>
+    <main>
+<center>
+<h1> ABSTRACT: -</h1>
+</center>
+<h2> Introduction</h2>
+		<div style="text-align: float-center; width:90%;padding:20px 70px;">
+        <p>
+		<h2 style="fontsize: 25px;"> sizeCloud networking is now the backbone of digital infrastructure, which reshapes the system of data storage, processing, and accessibility for organizations.</p></h2>
+		<p>
+		<h2 style="fontsize: 25px;">This step has given different businesses power and introduced various security challenges. Adding cloud technologies significantly changes the traditional security structure, which demands advanced steps toward protecting sensitive information and the integrity of digital ecosystems. In the cloud networking system, decreasing data storage and processing defects present a significant challenge </p></h2>
+		<p>
+		<h2 style="fontsize: 25px;">This distributed architecture increases the attack surface, which makes it compulsory to rethink the security measures to rely on the defenses. Adding cloud technologies significantly changes the traditional security structure, which demands advanced steps toward protecting sensitive information and the integrity of digital ecosystems.</p></h2>
+		<p>
+		<h2 style="fontsize: 25px;">In the cloud networking system, decreasing data storage and processing defects present a significant challenge Like previous on-site solutions where the information is managed within a controlled environment, cloud data is distributed across remote servers. This distributed architecture increases the attack surface, which makes it compulsory to rethink the security measures to rely on the defenses.</p></h2>
+		<p>
+		<h2 style="fontsize: 25px;">The unique nature of cloud environments, characterized by scalability, further complicates maintaining a good security posture. Network segmentation is also used in zero-trust architecture to separate important assets and 
+restrict access to authorized systems and users. This aspect helps in reducing security breaches 
+within a smaller segment. This makes it easy to respond to or detect any security incidents. 
+According to [10], network segmentation in ZTA provides many benefits. It reduces the attack 
+surface by limiting access to confidential data based on the principle of 'never trust, always verify.' 
+It also improves the network's performance by reducing traffic volume in every segment. This also 
+results in fast response time and low latency. Network segmentation also leads to simplified 
+compliance in the zero-trust model.<p/></h2>
+ => Key words: - Zero Trust Architecture in Cloud Networks: Application, Challenges and Future Opportunities
+</main>
+    <footer>
+        <p>&copy; 2024 My Website "Gohel Harsh"</p>
+    </footer>
+</body>
+</html>
